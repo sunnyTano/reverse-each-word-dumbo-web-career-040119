35f6e5b227034a3bba1ac48e70b2eb19965545f1
Susan Tan <ssusantan9@gmail.com>
2019-03-25 04:12:23 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,26 +7,8 @@ def reverse_each_word(str)
   newArr.join(" ")
 end
   
-def reverse_each_word_collect(string)
-  array = string.split(" ") #turn string into an array
-  test_array = []
-  array.collect do|string| #iterate over the array
-    test_array << string.reverse #reverse each word in the array
-  end
-  test_array.join(" ")
-end
-
-def reverse_each_word_with_each(string)
-  original_array = string.split(" ")
-  return_array = []
-  original_array.each do|string|
-    return_array << string.reverse
-  end
-  return_array.join(" ")
-end
-
-def reverse_each_word(string)
-  array = string.split(" ") #turn string into an array
+def reverse_each_word__with_collect(str)
+  array = str.split(" ") #turn string into an array
   test_array = []
   array.collect do|string| #iterate over the array
     test_array << string.reverse #reverse each word in the array
@@ -35,9 +17,7 @@ def reverse_each_word(string)
 end
 
 
-reverse_each_word("Hello there, and how are you?")
-  #=> "olleH ,ereht dna woh era ?uoy"
-  
+
 
   
 # describe '#reverse_each_word' do
